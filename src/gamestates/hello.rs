87a232a0553896b619e0cb4dc6f2ffd23730bcf0
Martin Billinger <flkazemakase@gmail.com>
2018-07-04 23:19:18 +0200

    feat: experimental tile drawing

--- src/gamestates/hello.rs
+++ src/gamestates/hello.rs
@@ -1,16 +1,45 @@
 
-use ggez::{graphics::{self, Point2}, Context, GameResult};
+use ggez::{graphics::{self, Image, Point2, Rect, spritebatch::SpriteBatch}, Context, GameResult};
 
 use super::GameState;
 
 pub struct HelloState {
     text: graphics::TextCached,
+    batch: SpriteBatch,
 }
 
 impl HelloState {
-    pub fn new(_ctx: &mut Context) -> GameResult<Self> {
+    pub fn new(ctx: &mut Context) -> GameResult<Self> {
+        let image = Image::new(ctx, "/tiles.png")?;
+        let mut batch = SpriteBatch::new(image);
+
+        batch.set_filter(graphics::FilterMode::Nearest);
+
+        batch.add(graphics::DrawParam {
+            src: Rect::new(37.0 / 256.0, 1.0 / 256.0, 16.0 / 256.0, 12.0 / 256.0),
+            dest: Point2::new(0.0, 0.0),
+            scale: Point2::new(5.0, 5.0),
+            .. Default::default()
+        });
+
+        batch.add(graphics::DrawParam {
+            src: Rect::new(37.0 / 256.0, 1.0 / 256.0, 16.0 / 256.0, 12.0 / 256.0),
+            dest: Point2::new(13.0 * 5.0, 6.0 * 5.0),
+            scale: Point2::new(5.0, 5.0),
+            .. Default::default()
+        });
+
+        batch.add(graphics::DrawParam {
+            src: Rect::new(37.0 / 256.0, 1.0 / 256.0, 16.0 / 256.0, 12.0 / 256.0),
+            dest: Point2::new(13.0 * 5.0, -6.0 * 5.0),
+            scale: Point2::new(5.0, 5.0),
+            .. Default::default()
+        });
+
+
         let s = HelloState {
             text: graphics::TextCached::new("Hello!")?,
+            batch,
         };
         Ok(s)
     }
@@ -23,6 +52,7 @@ impl GameState for HelloState {
 
     fn draw(&mut self, ctx: &mut Context) -> GameResult<()> {
         graphics::clear(ctx);
+        graphics::draw(ctx, &self.batch, Point2::new(100.0, 100.0), 0.0)?;
         graphics::draw(ctx, &self.text, Point2::new(512.0, 384.0), 0.0)?;
         graphics::present(ctx);
         Ok(())
